beb7b871a447982a8c8ac2380ed252cd7ec74195
Nikky Rojas <rojas7313@gmail.com>
2022-11-14 09:43:13 -0700

    first commit

---
+++ spec/services/tourist_site_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe TouristSiteService, type: :service do
+  describe 'Class Methods', :vcr do
+    describe '.get_sites' do
+      it 'returns a bunch of tourist sites based on lat long params' do
+        sites_hash = TouristSiteService.get_sites(30.03,31.23)
+        binding.pry
+        expect(sites_hash[:features].count).to eq 10
+        expect(sites_hash[:features]).to be_an Array
+        expect(sites_hash[:features][0]).to be_a Hash
+        expect(sites_hash[:features][0][:properties]).to be_a Hash
+        expect(sites_hash[:features][0][:properties][:name]).to be_a String
+      end
+    end
+  end
+end
