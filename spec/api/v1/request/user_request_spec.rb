481ffc0069a4d8dd6aff6c858e5a18d53be9b22f
Nikky Rojas <rojas7313@gmail.com>
2022-11-15 09:52:23 -0700

    added param missing edgecase

--- spec/api/v1/request/user_request_spec.rb
+++ spec/api/v1/request/user_request_spec.rb
@@ -44,6 +44,20 @@ RSpec.describe 'User Api' do
         expect(response.status).to eq(400)
         expect(response.body).to include("One or all of the following have occured: Passwords do not match, email already exist, or field is missing")
       end 
+      it 'return error if param is missing', :vcr do 
+        user_params = ({
+                        "email": "testing@example.com",
+                        "password": "test123",
+                        "password_confirmation": "teaD123"
+                      })
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        expect(response.body).to include("One or all of the following have occured: Passwords do not match, email already exist, or field is missing")
+      end 
     end
   end
 end
