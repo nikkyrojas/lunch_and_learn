10fc3f973a3da33cd9d4feb29c541c9e6f4ce65a
Nikky Rojas <rojas7313@gmail.com>
2022-11-15 13:38:18 -0700

    favorite serializer/request complete

---
+++ app/serializers/favorite_serializer.rb
@@ -0,0 +1,19 @@
+class FavoriteSerializer
+  def self.format_favorites(favorites)
+    {
+      data: favorites.map do |favorite| 
+        {
+          id: favorite.id,
+          type: 'favorite',
+          attributes: 
+            {
+              recipe_title: favorite.recipe_title,
+              recipe_link: favorite.recipe_link,
+              country: favorite.country,
+              created_at: favorite.created_at
+            }
+        }
+      end
+    }        
+  end
+end
