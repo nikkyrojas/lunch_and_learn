abfbb35c8d68346876d2530e8982637e4b3accc7
Nikky Rojas <rojas7313@gmail.com>
2022-11-15 09:28:19 -0700

    added user validations

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # before_create do self[:api_key] = api_key end
+
+  validates_presence_of :email, :name, :password, :api_key
+  validates_uniqueness_of :email
+
+  has_secure_token :api_key
+  has_secure_password 
+  
+  # def api_key
+  #   SecureRandom.hex(12)
+  # end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name } 
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :api_key }
+    it { should have_secure_password }
+  end
+end
